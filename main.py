1bdc7265becf56198284797af9ad5fd392bdc7cd
javiipzo <javipozo999@gmail.com>
2022-09-09 18:55:49 +0200

    main

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from Coche import coche
 from vehiculo import Vehiculo
+from Camioneta import camioneta
+from Bicicleta import Bicicleta
+from Motocicleta import Motocicleta
+def catalogar():
+    
+
+
+
+
 
 c = coche("azul", 4, 150, 1200)
-print(c)
+ca= camioneta("rojo", 6, 88, 1000, 2000)
+b= Bicicleta("rosa", 2, "urbana")
+m= Motocicleta("Negra",3, "deportiva",180, 1400)
+vehiculos=[c,ca,b,m]
